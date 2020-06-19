9ef5ca402563068b169f11242d320a2364d73ef5
taylor <taylor@affair.dev>
2020-06-19 14:51:36 -0400

    Added some @types libs for some packages for apollo and created apollo-server

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,31 +1,71 @@
 import "reflect-metadata";
 import {createConnection, getConnectionOptions} from "typeorm";
-// import {User} from "./entity/User";
-import { Place } from "./entity/Place";
+import express from 'express';
+import session from 'express-session';
+// @ts-ignore
+import connectSqlite3 from 'connect-sqlite3';
+import { ApolloServer } from "apollo-server-express";
+import * as path from 'path';
+import { buildSchema } from "type-graphql";
+import { PlaceResolver } from "./resolvers/PlaceResolver";
+
+const SQLiteStore = connectSqlite3(session);
 
 async function bootstrap() {
+    const app = express();
 
-    // Get config options from ormconfig.js
+    app.use(session({
+        store: new SQLiteStore({
+            db: 'database.sqlite',
+            concurrentDB: true
+        }),
+        name: 'qid',
+        secret: process.env.SESSION_SECRET || 'sakdnoasndoasdno',
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            maxAge: 1000 * 60 * 60 * 24 * 7 * 365
+        }
+    }))
+
+    // Get config options from typeorm-config.js
     const dbOptions = await getConnectionOptions(process.env.NODE_ENV || 'development');
 
-    createConnection({...dbOptions, name: 'default'}).then(async connection => {
+    createConnection({...dbOptions, name: 'default'}).then(async () => {
+        /**
+         * Build Server Schema
+         */
+        const schema = await buildSchema({
+            resolvers: [PlaceResolver],
+            validate: true,
+            emitSchemaFile: path.resolve(__dirname, 'schema.graphql')
+        });
 
-        console.log("Inserting a new user into the database...");
-        const place = new Place();
-        place.id = 1;
-        place.title = 'New York City';
-        place.description = 'The Big Apple';
-        place.imageUrl = 'https://picsum.photos/700';
-        place.creationDate = new Date();
-        await connection.manager.save(place);
-        const places = await connection.manager.find(Place);
-        console.log("Loaded places: " + places);
-
-
-        console.log("Here you can setup and run express/koa/any other framework.");
+        /**
+         * Create Apollo Server instance
+         */
+        const apolloServer = new ApolloServer({
+            schema,
+            context: ({ req, res }) => ({ req, res }),
+            introspection: true,
+            playground: true
+        });
+        /**
+         * Apply server instance as middleware
+         */
+        apolloServer.applyMiddleware({ app, cors: true });
+        /**
+         * Port
+         */
+        const port : string | number = process.env.PORT || 4000;
+        app.listen(port, () => {
+            console.log(`Server started at http://localhost:${port}`);
+        });
 
     }).catch(error => console.log(error));
 }
 
-bootstrap();
+bootstrap().then().catch((error) => console.log(error.stack));
 
--- api/src/resolvers/PlaceResolver.ts
+++ api/src/resolvers/PlaceResolver.ts
@@ -1,10 +1,16 @@
-import { Resolver, Query, Arg, Mutation } from "type-graphql";
+import { Resolver, Query, Arg } from "type-graphql";
 // import { plainToClass } from "class-transformer";
 import { Place } from "../entity/Place";
 // import { PlaceInput } from "../graphql-types/PlaceInput";
 
 @Resolver(() => Place)
 export class PlaceResolver {
+    /**
+     * @Query assume certain properties on a given class or function
+     * This function is to find One Place in the database
+     * @param id
+     * @return Place
+     */
     @Query(() => Place, { nullable: true })
     async place(@Arg('id') id: number): Promise<Place | undefined> {
         return await Place.findOne(id)
